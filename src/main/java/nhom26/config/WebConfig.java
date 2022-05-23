d88c20a7ef96b1f87fe210bdaf08c08523984a6f
TaPDuy <tpduy2001@gmail.com>
2022-05-23 18:10:07 +0700

    Fixed login/signin's style

--- src/main/java/nhom26/config/WebConfig.java
+++ src/main/java/nhom26/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -61,7 +62,11 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll().and().requestCache().disable();
     }
-
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjars/**");
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
