d1ba04e49b40e44f49fab376ddf8fb63c7d48374
CuongNM <manhcuong0978481376@gmail.com>
2022-05-27 23:10:15 +0700

    add search post

--- src/main/java/nhom26/service/impl/PostServiceImp.java
+++ src/main/java/nhom26/service/impl/PostServiceImp.java
@@ -6,10 +6,15 @@ import nhom26.repository.PostRepository;
 import nhom26.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImp implements PostService {
@@ -49,4 +54,12 @@ public class PostServiceImp implements PostService {
     private int subtractPageByOne(int page){
         return (page < 1) ? 0 : page - 1;
     }
+
+	@Override
+	public Page<Post> findAllByKeyWord(String keyword) {
+		List<Post> posts = postRepository.findAll().stream().filter(p -> p.getTitle().toLowerCase().contains(keyword.toLowerCase())).collect(Collectors.toList());
+		
+		
+		return new PageImpl<>(posts);
+	}
 }
